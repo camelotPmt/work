c2b09debb30cfcf9fa6383da7f8708a0af62cc9b
daiyang <daiyang@camelotchina.com>
2018-05-04 19:17:32 +0800

    init project

---
+++ project-contribute-server/src/main/java/com/camelot/Application.java
@@ -0,0 +1,15 @@
+package com.camelot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by daiyang on 2018/5/4.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ project-contribute-server/src/main/java/com/camelot/controller/DomeController.java
@@ -0,0 +1,19 @@
+package com.camelot.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by daiyang on 2018/5/4.
+ */
+@RestController
+@RequestMapping("/dome")
+public class DomeController {
+
+    @GetMapping("")
+    public ResponseEntity dome(){
+        return ResponseEntity.ok("请求成功");
+    }
+}
