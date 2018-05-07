c16fc207e08fd331ad1ae50caa4f8f6804b344d7
lianxue <lianxuelx_v@didichuxing.com>
2018-05-07 17:02:14 +0800

    左侧图标

--- project-contribute-web/src/components/SystemManagemont/sysPages/sysLeftNav.vue
+++ project-contribute-web/src/components/SystemManagemont/sysPages/sysLeftNav.vue
@@ -2,31 +2,31 @@
     <div class="sys-nav-cont">
         <Menu active-name="1">
             <MenuItem name="1">
-                <Icon type="document-text"></Icon>
+                <i class="iconfont icon-yonghu"></i>
                 <router-link to="/systemManager">用户管理</router-link>
             </MenuItem>
             <MenuItem name="2">
-                <Icon type="document-text"></Icon>
+                <i class="iconfont icon-navicon-jsgl"></i>
                 <router-link to="/roleManagement">角色管理</router-link>
             </MenuItem>
             <MenuItem name="3">
-                <Icon type="document-text"></Icon>
+                <i class="iconfont icon-quanxianguanli"></i>
                 <router-link to="/authManagement">权限管理</router-link>
             </MenuItem>
             <MenuItem name="4">
-                <Icon type="document-text"></Icon>
+                <i class="iconfont icon-zuzhijiagou"></i>
                 <router-link to="">组织架构</router-link>
             </MenuItem>
             <MenuItem name="5">
-                <Icon type="document-text"></Icon>
+                <i class="iconfont icon-zhaoshengshujuzidian"></i>
                 <router-link to="">数据库字典</router-link>
             </MenuItem>
             <MenuItem name="6">
-                <Icon type="document-text"></Icon>
+                <i class="iconfont icon-xitongceshiyongli"></i>
                 <router-link to="">用例库</router-link>
             </MenuItem>
             <MenuItem name="7">
-                <Icon type="document-text"></Icon>
+                <i class="iconfont icon-caozuorizhi"></i>
                 <router-link to="">操作日志</router-link>
             </MenuItem>
         </Menu>
@@ -44,7 +44,7 @@
             height:50px;
             line-height:50px;
         }
-        .ivu-icon{position: absolute; top:15px; font-size:20px; left:20px;}
+        .iconfont{position: absolute; top:0; font-size:16px; left:20px;}
         a{ display:block; padding-left:50px;}
     }
 </style>
