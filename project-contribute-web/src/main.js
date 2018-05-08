db4a62a46a53cf0351597433a9580ea95e9710c8
sunjiankun <sjk1695507228@163.com>
2018-05-08 17:09:41 +0800

    引入moment依赖

--- project-contribute-web/src/main.js
+++ project-contribute-web/src/main.js
@@ -3,22 +3,24 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-//import Routers from './router.js';
-//iview
-import iView from 'iview';
-import 'iview/dist/styles/iview.css';
-import './my-theme/index.less';
-import './my-theme/index.css';
-import './fonts/iconfont.css';
-import { HTTP } from './util/axios'
-import { API } from './util/axios'
+// import Routers from './router.js';
+// iview
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
+import './my-theme/index.less'
+import './my-theme/index.css'
+import './fonts/iconfont.css'
+import { API, HTTP } from './util/axios'
 import ZkTable from 'vue-table-with-tree-grid'
-Vue.prototype.$http = HTTP;
-Vue.prototype.$api = API;
-Vue.use(iView);
+import Moment from 'moment'
+Vue.prototype.$http = HTTP
+Vue.prototype.$api = API
+Vue.prototype.$moment = Moment
+Vue.use(iView)
 Vue.config.productionTip = false
 Vue.use(ZkTable)
-//Vue.use(VueRouter);
+
+// Vue.use(VueRouter);
 
 /* eslint-disable no-new */
 new Vue({
