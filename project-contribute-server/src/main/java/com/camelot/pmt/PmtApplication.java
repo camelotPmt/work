48461ece3bbd3fb1e39821b1194628c45a10ff5b
mztyd <17090023169@163.com>
2018-05-08 16:26:59 +0800

    update page

--- project-contribute-server/src/main/java/com/camelot/pmt/PmtApplication.java
+++ project-contribute-server/src/main/java/com/camelot/pmt/PmtApplication.java
@@ -13,5 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PmtApplication {
     public static void main(String[] args) {
         SpringApplication.run(PmtApplication.class, args);
+        //
     }
 }
