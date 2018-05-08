06926532b67335510afdacc6abec100d021f01d2
956772658@qq.com <956772658@qq.com>
2018-05-08 15:36:17 +0800

    fangjingyao测试权限

--- project-contribute-server/src/main/java/com/camelot/pmt/PmtApplication.java
+++ project-contribute-server/src/main/java/com/camelot/pmt/PmtApplication.java
@@ -11,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PmtApplication {
+    /*
+    * */
     public static void main(String[] args) {
         SpringApplication.run(PmtApplication.class, args);
     }
