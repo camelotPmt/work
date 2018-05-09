07727e15069fd5bdd3fa1bce3b7ed9cd8c70f5f3
956772658@qq.com <956772658@qq.com>
2018-05-09 16:31:51 +0800

    提交生成代码

---
+++ project-contribute-server/src/main/java/com/camelot/pmt/mapper/SysGroupMapper.java
@@ -0,0 +1,42 @@
+package com.camelot.pmt.mapper;
+
+
+import com.camelot.pmt.model.SysGroup;
+
+public interface SysGroupMapper {
+    /**
+     *
+     * @mbggenerated
+     */
+    int deleteByPrimaryKey(Integer id);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    int insert(SysGroup record);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    int insertSelective(SysGroup record);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    SysGroup selectByPrimaryKey(Integer id);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    int updateByPrimaryKeySelective(SysGroup record);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    int updateByPrimaryKey(SysGroup record);
+}
---
+++ project-contribute-server/src/main/java/com/camelot/pmt/mapper/SysUserGroupMapper.java
@@ -0,0 +1,42 @@
+package com.camelot.pmt.mapper;
+
+
+import com.camelot.pmt.model.SysUserGroup;
+
+public interface SysUserGroupMapper {
+    /**
+     *
+     * @mbggenerated
+     */
+    int deleteByPrimaryKey(Integer id);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    int insert(SysUserGroup record);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    int insertSelective(SysUserGroup record);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    SysUserGroup selectByPrimaryKey(Integer id);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    int updateByPrimaryKeySelective(SysUserGroup record);
+
+    /**
+     *
+     * @mbggenerated
+     */
+    int updateByPrimaryKey(SysUserGroup record);
+}
