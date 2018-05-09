8774695635664b7964902fe78e509b602269fcff
xiaoqiaoqiao888 <qiaodongjie666@163.com>
2018-05-09 17:46:15 +0800

    格式化

--- project-contribute-server/src/main/java/com/camelot/pmt/PmtApplication.java
+++ project-contribute-server/src/main/java/com/camelot/pmt/PmtApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- *
  * @author daiyang
  * @date 2018/5/4
- * 
  */
 
 @SpringBootApplication
